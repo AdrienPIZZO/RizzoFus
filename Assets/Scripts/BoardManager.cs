a9aafd0e5b00861d532225ff4c73ef9af5f309cb
Vincent <vincent.ribot@free.fr>
2020-03-19 01:29:22 +0100

    Board

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+    private const int NB_TILES = 8;
+
+    private int selectionX = -1;
+    private int selectionY = -1;
+
+    public List<GameObject> chosensPrefabs;
+    public List<GameObject> activeChosen;
+
+    private void Update()
+    {
+        UpdateSelection();
+        DrawBoard();
+    }
+
+    private void UpdateSelection()
+    {
+        if(!Camera.main)
+            return;
+        
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Plane"))){
+            Debug.Log(hit.point);
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+    }
+ 
+    private void DrawBoard()
+    {
+        Vector3 widthLine = Vector3.right * TILE_SIZE * NB_TILES;
+        Vector3 heigthLine = Vector3.forward * TILE_SIZE * NB_TILES;
+    
+        for(int i=0; i<=NB_TILES; i++){
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            for(int j=0; j<=NB_TILES; j++){
+                start = Vector3.right * i;
+                Debug.DrawLine(start, start + heigthLine);
+            }
+        }
+
+        // Draw selection
+        if(selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(Vector3.forward * selectionY + Vector3.right * selectionX,
+            Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1));
+        }
+    }
+
+}
+
